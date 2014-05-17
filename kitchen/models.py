6164d77e7a60d186ee7e66d19ddb693539283ece
Kirill Shmakov <shmakir@yandex.ru>
2014-05-17 15:35:08 +0400

    Projects table created and registered in admin interface.

--- kitchen/models.py
+++ kitchen/models.py
@@ -2,4 +2,14 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Project(models.Model):
+    id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User)
+    project_name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    last_access = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '"{0}" created {1:%Y-%m-%d %H:%M} UTC'.format(self.project_name, self.created)
+
+    class Meta:
+        db_table='kitchen_project'
